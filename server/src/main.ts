7f2ff16204456708ed7a8e61421d29f693087bba
thdwlsgus0 <thdwlsgus0@naver.com>
2024-07-23 02:03:28 +0900

    chore: hot-loading 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 4000;
   await app.listen(port);
-  console.log(port);
+  console.log(`listeningd on port: ${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
+
 bootstrap();
