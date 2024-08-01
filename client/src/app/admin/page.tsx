681f853781f6f6838147b3aadb3b6fc4581bc2a6
skrgus6269 <knh6269@gmail.com>
2024-08-01 12:52:04 +0900

    feat: 관리자 페이지 추가 (#5)

---
+++ client/src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/*
+이름: 관리자 페이지
+역할: 멤버들의 벙 참여 통계 확인 페이지
+*/
+export default function Admin() {
+  return (
+    <div>
+      <h1>Welcome to the Admin Page</h1>
+    </div>
+  );
+}
