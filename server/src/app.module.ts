52526a4ee343c98aec7bb674a755f9c88ca6fcfb
thdwlsgus0 <thdwlsgus0@naver.com>
2024-08-16 23:50:08 +0900

    chore: auth 모듈, 서비스, 컨트롤러 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UserModule } from './user/user.module';
 import { PlanModule } from './plan/plan.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, PlanModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, PlanModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
