2658a9e90ff027b30beb67368a85fa4052e36a0f
skrgus6269 <knh6269@gmail.com>
2024-08-18 18:22:09 +0900

    feat: 린터 변경 (#13)

--- client/src/apis/fetcher.ts
+++ client/src/apis/fetcher.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NEXT_PUBLIC_API_HOST } from "@constants/develop.constants";
 
-const fetchers = axios.create({
+const fetcher = axios.create({
   baseURL: NEXT_PUBLIC_API_HOST,
   timeout: 2500,
 });
